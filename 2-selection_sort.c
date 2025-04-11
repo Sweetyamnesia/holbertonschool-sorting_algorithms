0208007b7c1161580a04e7ff9d09a075dd862b60
Lucas-blancaws <lblancportier@gmail.com>
2025-04-11 15:27:44 +0200

    fix task 3

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,19 +8,6 @@ int _putchar(char c)
 {
     return (write(1, &c, 1));
 }
-void print_number(int n)
-{
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-
-    if (n / 10)
-        print_number(n / 10);
-
-    _putchar(n % 10 + '0');
-}
 
 /**
  * selection_sort - tri un tableau d'entiers en ordre croissant
@@ -30,11 +17,13 @@ void print_number(int n)
 void selection_sort(int *array, size_t size)
 {
     int temp;
-    size_t i, min_index; 
-    size_t k; 
+    size_t i, min_index;
     size_t j;
 
-    for (i = 0; i < size - 1; i++)
+    if (array == NULL || size < 2)
+    return;
+
+   for (i = 0; i < size - 1; i++)
     {
         min_index = i;
 
@@ -50,15 +39,7 @@ void selection_sort(int *array, size_t size)
             temp = array[i];
             array[i] = array[min_index];
             array[min_index] = temp;
-            for (k = 0; k < size; k++) 
-            {
-                print_number(array[k]);
-                if (k < size - 1) 
-                {
-                    _putchar(' ');
-                }
-            }
-            _putchar('\n');
+            print_array(array, size);
         }
     }
 }
